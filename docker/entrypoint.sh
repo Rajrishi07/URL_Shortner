b02059e50cd660a9d5522e55f3e801b9cd2c96d9
Rajrishi Charan <130096898+Rajrishi07@users.noreply.github.com>
2026-07-23 11:50:43 +0530

    Update entrypoint.sh for prod

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+echo "Running database migrations..."
 alembic upgrade head
 
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --no-access-log --reload
+echo "Starting FastAPI..."
+
+exec uvicorn app.main:app \
+    --host 0.0.0.0 \
+    --port ${PORT:-8000} \
+    --no-access-log
